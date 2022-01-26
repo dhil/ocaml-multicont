80c30eaf2a1b96f5f0c7d2c27b59fd9c1903e106
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2022-01-26 18:33:17 +0000

    Modular rollback parsing example

---
+++ examples/rollback.ml
@@ -0,0 +1,96 @@
+(* Modular rollback parsing. Adapted from Lindley et al. (2017),
+   c.f. https://arxiv.org/pdf/1611.09259.pdf *)
+
+open Effect
+
+type _ eff += Peek : char eff
+              | Accept : unit eff
+
+exception Abort
+
+let peek : unit -> char
+  = fun () -> perform Peek
+
+let accept : unit -> unit
+  = fun () -> perform Accept
+
+let abort : unit -> 'a
+  = fun () -> raise Abort
+
+type 'a log = Start of (unit, 'a) Multicont.Shallow.resumption
+            | Inched of 'a log * (char, 'a) Multicont.Shallow.resumption
+            | Ouched of 'a log
+
+
+let identity : ('a, 'a) Shallow.handler
+  = { retc = (fun x -> x)
+    ; exnc = (fun e -> raise e)
+    ; effc = (fun (type a) (_ : a eff) -> None) }
+
+let rec input : 'a log -> char option -> ('a, 'a) Shallow.handler
+  = fun l buf ->
+  let open Shallow in
+  { retc = (fun x -> x)
+  ; exnc = (function Abort -> rollback l | e -> raise e)
+  ; effc = (fun (type a) (eff : a eff) ->
+    match eff with
+    | Peek -> Some (fun (k : (a, _) continuation) ->
+                  let open Multicont.Shallow in
+                  let r = promote k in
+                  match buf with
+                  | Some c -> resume_with r c (input l buf)
+                  | None -> match input_char stdin with
+                            | '\b' -> rollback l
+                            | c -> resume_with r c (input (Inched (l, r)) (Some c)))
+    | Accept -> Some (fun (k : (a, _) continuation) ->
+                  let open Multicont.Shallow in
+                  let r = promote k in
+                  match buf with
+                  | Some c -> output_char stdout c; resume_with r () (input (Ouched l) None)
+                  | None -> resume_with r () (input l None))
+    | _ -> None) }
+and rollback : 'a log -> 'a = function
+  | Start p -> parse p
+  | Ouched l -> output_char stdout '\b';
+                output_char stdout ' ';
+                output_char stdout '\b';
+                rollback l
+  | Inched (l, r) ->
+     let open Multicont.Shallow in
+     (* Memory leak *)
+     let f = promote (Shallow.fiber (fun () ->
+                          resume_with r (peek ()) identity))
+     in
+     resume_with f () (input l None)
+and parse : (unit, 'a) Multicont.Shallow.resumption -> 'a
+  = fun r ->
+  let open Multicont.Shallow in
+  resume_with r () (input (Start r) None)
+
+let rec zeros : int -> int
+  = fun n ->
+  match peek () with
+  | '0' -> accept(); zeros (n+1)
+  | ' ' -> accept(); n
+  | c -> abort()
+
+let t1 () =
+  let open Multicont.Shallow in
+  parse (promote (Shallow.fiber (fun () -> zeros 0)))
+
+let rec nest : char list -> int -> char list
+  = fun cs n ->
+  if n = 0
+  then match peek () with
+       | '('  -> accept (); nest cs 1
+       | '\n' -> accept (); cs
+       | c    -> abort ()
+  else match peek () with
+       | '(' -> accept (); nest cs (n + 1)
+       | ')' -> accept (); nest cs (n - 1)
+       | c   -> accept (); nest (c :: cs) n
+
+let t2 () =
+  let open Multicont.Shallow in
+  let cs = parse (promote (Shallow.fiber (fun () -> nest [] 0))) in
+  String.init (List.length cs) (fun i -> List.nth cs i)
