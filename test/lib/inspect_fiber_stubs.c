781d2d1918d56cd34488ad7a49243a1641d20045
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-03-19 10:32:35 +0100

    Simplify stubs

--- test/lib/inspect_fiber_stubs.c
+++ test/lib/inspect_fiber_stubs.c
@@ -1,17 +1,7 @@
 #define CAML_INTERNALS
 
-#include <caml/mlvalues.h> // provides basic CAML macros and type definitions
-#include <caml/fail.h>     // provides [caml_raise_out_of_memory]
-#include <caml/alloc.h>    // provides [caml_alloc_1]
 #include <caml/fiber.h>    // provides Stack_* macros, [struct stack_info]
 #include <caml/memory.h>   // provides CAMLparam* and CAMLreturn* macros
-#include <caml/misc.h>     // provides [CAMLnoalloc] macro
-#include <caml/version.h>  // provides OCaml versioning macros
-
-#ifdef NATIVE_CODE
-#include <caml/stack.h>
-#include <caml/frame_descriptors.h>
-#endif
 
 CAMLextern value caml_copy_int64 (int64_t); // defined in [ocaml/runtime/ints.c]
 
